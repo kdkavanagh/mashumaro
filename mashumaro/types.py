0f620c225ca777dbfa576828fec81966d9ffbfd7
Kyle D. Kavanagh <kdkavanagh@gmail.com>
2026-03-12 21:24:04 -0500

    Add ability to enumerate possible subtypes to deserialize to.  Fixes #281

--- mashumaro/types.py
+++ mashumaro/types.py
@@ -1,7 +1,7 @@
 import decimal
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Any, Optional, Type, Union
+from typing import Any, Iterable, Optional, Type, Union
 
 from typing_extensions import Literal
 
@@ -109,6 +109,7 @@ class Discriminator:
     include_subtypes: bool = False
     include_current_type: bool = False
     variant_tagger_fn: Optional[Callable[[Any], Any]] = None
+    possible_subtypes_fn: Optional[Callable[[], Iterable[Type]]] = None
 
     def __post_init__(self) -> None:
         if not self.include_supertypes and not self.include_subtypes:
